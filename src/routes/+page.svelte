210f84cacf20011ae60d91fb7c2fd4fcac80cafe
syuzu <syuuzu@proton.me>
2024-01-23 22:45:53 -0500

    add link to jellyfin instance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <body>
-<h1>syuzu.moe</h1>
-<p>holy moly!</p>
+<h1>syuzu<a href="https://media.syuzu.moe"><img src="sleepyOneko.gif" width="35" height="35" class= "inline mt-2.5 mr-1"></a>moe</h1>
+<div>
+    <p class="my-0">
+        
+       snoozu...
+    </p>
+</div>
 </body>
