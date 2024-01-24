806cb5d7e84197100c419862e6f55794546ddc62
syuzu <syuuzu@proton.me>
2024-01-24 18:03:31 -0500

    update alt text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <body>
-<h1>syuzu<a href="https://media.syuzu.moe"><img alt="oneko" src="sleepyOneko.gif" width="35" height="35" class= "inline mt-2.5 mr-1"></a>moe</h1>
+<h1>syuzu<a href="https://media.syuzu.moe"><img alt="pm for login info" src="sleepyOneko.gif" width="35" height="35" class= "inline mt-2.5 mr-1"></a>moe</h1>
 <div>
     <h3>
        snoozu...
